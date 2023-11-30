0829aa2a04123e05e8932bc8315663d30ecd34ee
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-30 20:21:09 +0100

    task-2-start

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
