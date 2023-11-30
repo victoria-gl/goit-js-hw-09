1f9c3f3e57c4ab80260b3e9078dfd71a89826052
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-30 19:59:48 +0100

    task-01-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const buttonStart = document.querySelector(".button-start");
+const buttonStop = document.querySelector(".button-stop");
+const bodyBtn = document.querySelector("body")
+
+buttonStart.addEventListener("click", handleStart);
+buttonStop.addEventListener("click", handleStop);
+
+buttonStop.setAttribute("disabled", "");
+
+function handleStart(event) {
+    if (event.target.nodeName !== "BUTTON") {
+        return;
+      }
+    
+    timerId = setInterval(() => {
+        bodyBtn.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    buttonStart.setAttribute("disabled", "");
+    buttonStop.removeAttribute("disabled");
+}
+
+function handleStop(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+  clearInterval(timerId);
+  buttonStop.setAttribute("disabled", "");
+  buttonStart.removeAttribute("disabled");
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+            .toString(16)
+            .padStart(6, 0)}`;
+   }
+
+
+
+
+
+
 
